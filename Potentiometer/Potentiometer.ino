ba4ca3a929ae91cf774572c4c4616a7eec43d498
yklovejesit config --global user.email yklovejesus@gmail.com <yklovejesus@gmail.com>
2020-10-18 00:21:34 +0900

    UPDATE:Porentiometer

--- Potentiometer/Potentiometer.ino
+++ Potentiometer/Potentiometer.ino
@@ -1,7 +1,7 @@
 void setup() {
     Serial.begin(9600); // Serial 통신을 초기화 합니다.
 }
-
+//가변 저항
 void loop() {
     int readValue = analogRead(A0); // A0로 받은 아날로그 값을 readValue변수로 넘겨줍니다.
     Serial.println(readValue); // Serial 모니터에 readValue 값을 찍어주고 한줄 내려줍니다.
